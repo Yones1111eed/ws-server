42fbb5c6730439ee5862ae0d9aaab82c9ccecc0e
Yones1111eed <156520701+Yones1111eed@users.noreply.github.com>
2026-02-08 12:35:32 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({
+  port: process.env.PORT || 10000
+});
+
+wss.on("connection", ws => {
+  console.log("Client connected");
+
+  ws.on("message", msg => {
+    console.log(msg.toString());
+    wss.clients.forEach(c => c.send(msg.toString()));
+  });
+});
+
+console.log("WebSocket running");
